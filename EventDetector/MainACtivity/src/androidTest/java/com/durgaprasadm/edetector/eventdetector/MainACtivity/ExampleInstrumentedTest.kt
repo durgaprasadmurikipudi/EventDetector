0cd34b605a5433bd7eba2869ed9efd315afbd53d
Durga Prasad Murikipudi <durgaprasadmurikipudi@gmail.com>
2020-03-06 22:31:43 +0530

    Event detector app

---
+++ EventDetector/MainACtivity/src/androidTest/java/com/durgaprasadm/edetector/eventdetector/MainACtivity/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package com.durgaprasadm.edetector.eventdetector.MainACtivity
+
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+        assertEquals("com.durgaprasadm.edetector.eventdetector.MainACtivity.test", appContext.packageName)
+    }
+}
---
+++ EventDetector/MainACtivity/src/main/java/com/durgaprasadm/edetector/eventdetector/MainACtivity/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.durgaprasadm.edetector.eventdetector.MainACtivity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
